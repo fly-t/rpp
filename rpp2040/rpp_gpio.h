0e14315b3d593b8a2fe539a0d88752083103b39f
dd21 <dd21@163.com>
2023-03-05 16:43:01 +0800

    update annotation

--- rpp2040/rpp_gpio.h
+++ rpp2040/rpp_gpio.h
@@ -3,8 +3,21 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+/**
+* @brief init gpio, with PIN and Direction
+*
+* @param[gpio_pin] PIN num
+* @param[gpio_dir] direction is GPIO_OUT or GPIO_IN
+* @return none
+*/
 void rpp_gpio_init(uint gpio_pin, uint gpio_dir);
 
+/**
+* @brief make level toggle
+*
+* @param[gpio_pin] PIN num
+* @return none
+*/
 void rpp_gpio_toggle(uint gpio_pin);
 
 #endif // __RPP_GPIO_H
